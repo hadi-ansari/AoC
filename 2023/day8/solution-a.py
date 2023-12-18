f58323f7c1eb7e12f000e25614dc210e1fdb452c
Hadi Ansari <hadi.ansari@axis.com>
2023-12-19 00:15:01 +0100

    refactor day8 part a

--- 2023/day8/solution-a.py
+++ 2023/day8/solution-a.py
@@ -1,28 +1,27 @@
+from reader import read_problem
 import re
 
-input_file = open("input-real.txt")
+def main():
+    content = read_problem("input.txt")
+    instructions = content[0].split()[0]
+    road_graph = {}
 
-content = input_file.readlines()
+    for i in range(len(content)):
+        if not i < 2:
+            splited_line = content[i].split("=")
+            road_graph[splited_line[0].strip()] = tuple(re.findall('[A-Z]{3}', splited_line[1]))
 
-input_file.close()
+    current_position = 'AAA'
+    steps = 0
+    while current_position != 'ZZZ':
+        for instruction in instructions:
+            if instruction == "L":
+                current_position = road_graph[current_position][0]
+            else:
+                current_position = road_graph[current_position][1]
+            steps += 1
+        
+    print(steps)
 
-instructions = content[0].split()[0]
 
-road_graph = {}
-
-for i in range(len(content)):
-    if not i < 2:
-        splited_line = content[i].split("=")
-        road_graph[splited_line[0].strip()] = tuple(re.findall('[A-Z]{3}', splited_line[1]))
-
-current_position = 'AAA'
-steps = 0
-while current_position != 'ZZZ':
-    for instruction in instructions:
-        if instruction == "L":
-            current_position = road_graph[current_position][0]
-        else:
-            current_position = road_graph[current_position][1]
-        steps += 1
-    
-print(steps)
+main()
