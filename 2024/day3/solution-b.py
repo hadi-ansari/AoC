db5ac17caa025f1368639c045db146ef34952136
Hadi Ansari <hadia@axis.com>
2024-12-03 18:29:41 +0100

    2024: solved day3 part two

--- 2024/day3/solution-b.py
+++ 2024/day3/solution-b.py
@@ -6,15 +6,34 @@ def multiply(exp):
     res = int(digits[0]) * int(digits[1])
     return res
 
-def main():
-    curropted_memory = read_problem("input.txt")
-
+def find_multiplies(exp):
+    multiplies = re.findall("mul\(\d{1,3},\d{1,3}\)", exp)
     sum = 0
+    for m in multiplies:
+        sum += multiply(m)
 
-    for line in curropted_memory:
-        matches = re.findall("mul\(\d{1,3}\,\d{1,3}\)", line)
-        for match in matches:
-            sum += multiply(match)
+    return sum
+
+def main():
+    lines = read_problem("input.txt")
+
+    content = ""
+    for l in lines:
+        content += l
+
+    # do().*?don't()
+
+    content = content.replace("\n", "")
+    sum = 0
+    matches_first_part = re.findall("(^.*?(do\(\)|don\'t\(\)))", content)
+
+    if len(matches_first_part) > 0:
+        sum += find_multiplies(matches_first_part[0][0])
+    matches_do = re.findall("do\(\).*?don\'t\(\)", content)
+   
+
+    for match in matches_do:
+        sum += find_multiplies(match)
 
     print("sum => ", sum)
 
