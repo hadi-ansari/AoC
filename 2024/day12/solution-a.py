a490cf791831bfeaa0359be318f638ea8bf95b78
Hadi Ansari <hadia@axis.com>
2024-12-14 21:30:18 +0100

    2024: refined day 12 solution

--- 2024/day12/solution-a.py
+++ 2024/day12/solution-a.py
@@ -1,5 +1,8 @@
 from reader import read_problem
 
+Y = 0
+X = 1
+
 def find_neighbours(map, y, x):
     neigbours = []
     if y < len(map) - 1 and map[y + 1][x] == map[y][x]:
@@ -25,32 +28,27 @@ def find_number_of_different_regions(map, y, x):
         sum+=1
     return sum
 
-def calculate_area(region):
-    return len(region)
-
 def calculate_perimeter_easy(map, region):
     sum = 0
-    for i in region:
-        sum += find_number_of_different_regions(map, i[0], i[1])
+    for pos in region:
+        sum += find_number_of_different_regions(map, pos[Y], pos[X])
 
     return sum
 
 def find_region(map, y, x):
     queue = []
-    queue.append((y, x))
     visited = set()
+    queue.append((y, x))
     visited.add((y,x))
-    permiter = 0
 
     while len(queue) > 0:
         pos = queue.pop(0)
-        neighbours = find_neighbours(map, pos[0], pos[1])
+        neighbours = find_neighbours(map, pos[Y], pos[X])
 
         for n in neighbours:
             if n not in visited:
                 visited.add(n)
                 queue.append(n)
-
 
 
     permiter = calculate_perimeter_easy(map, visited)
@@ -66,8 +64,7 @@ def main():
             if (y, x) not in visited:
                 new_visited, per = find_region(map, y, x)
                 visited = visited | new_visited
-                area = calculate_area(new_visited)
-                sum += (per * area)
+                sum += (per * len(new_visited))
 
     print("sum is => ", sum)
 
--- 2024/day12/solution-b.py
+++ 2024/day12/solution-b.py
@@ -1,4 +1,11 @@
 from reader import read_problem
+from enum import Enum
+
+# TOP RIGHT DOWN LEFT
+DIRS = Enum('DIRS', ["UP", "RIGHT", "DOWN", "LEFT"])
+
+Y = 0
+X = 1
 
 def find_neighbours(map, y, x):
     neigbours = []
@@ -38,68 +45,70 @@ def find_number_of_different_regions(map, y, x):
         sum+=1
     return sum
 
-def not_same_region(map, dir, y, x):
-    if dir == "UP" and ((y >= 1 and map[y - 1][x] != map[y][x]) or y == 0):
-        return True
-    if dir == "RIGHT" and( (x < len(map[y]) - 1 and map[y][x + 1] != map[y][x]) or x == len(map[y]) -1):
+def not_same_region(map, dir, pos):
+    max_y_idx = len(map) - 1
+    max_x_idx = len(map[0]) - 1
+    current_region = map[pos[Y]][pos[X]]
 
+    if dir == DIRS.UP and (pos[Y] >= 1 and map[pos[Y] - 1][pos[X]] != current_region or pos[Y] == 0):
         return True
-    if dir == "DOWN" and ((y < len(map) - 1 and map[y + 1][x] != map[y][x]) or y == len(map) - 1):
-
+    
+    if dir == DIRS.RIGHT and (pos[X] < max_x_idx and map[pos[Y]][pos[X] + 1] != current_region or pos[X] == max_x_idx):
         return True
-    if dir == "LEFT" and ((x >= 1 and map[y][x - 1] != map[y][x]) or x == 0):
-
+    
+    if dir == DIRS.DOWN and (pos[Y] < max_y_idx and map[pos[Y] + 1][pos[X]] != current_region or pos[Y] == max_y_idx):
+        return True
+    
+    if dir == DIRS.LEFT and (pos[X] >= 1 and map[pos[Y]][pos[X] - 1] != current_region or pos[X] == 0):
         return True
     
     return False
 
-def calculate_area(region):
-    return len(region)
-
-def get_corners(map, pos, visited):
+def get_corners(map, pos, visited_corners):
     sum_corners = 0
-    if not_same_region(map, "UP", pos[0], pos[1]) and not_same_region(map, "LEFT", pos[0], pos[1]):
+
+    if not_same_region(map, DIRS.UP, pos) and not_same_region(map, DIRS.LEFT, pos):
         sum_corners += 1
-    if not_same_region(map, "UP", pos[0], pos[1]) and not_same_region(map, "RIGHT", pos[0], pos[1]):
+    if not_same_region(map, DIRS.UP, pos) and not_same_region(map, DIRS.RIGHT, pos):
         sum_corners += 1
-    if not_same_region(map, "DOWN", pos[0], pos[1]) and not_same_region(map, "LEFT", pos[0], pos[1]):
+    if not_same_region(map, DIRS.DOWN, pos) and not_same_region(map, DIRS.LEFT, pos):
         sum_corners += 1
-    if not_same_region(map, "DOWN", pos[0], pos[1]) and not_same_region(map, "RIGHT", pos[0], pos[1]):
+    if not_same_region(map, DIRS.DOWN, pos) and not_same_region(map, DIRS.RIGHT, pos):
         sum_corners += 1
 
     # Outer corners
     # _|
-    if not_same_region(map, "UP", pos[0], pos[1]) and (is_neighbour(map, pos[0], pos[1], pos[0], pos[1] + 1) and is_neighbour(map, pos[0], pos[1] + 1, pos[0] - 1, pos[1] + 1)):
-        if ((pos[0], pos[1]), (pos[0] , pos[1] + 1), (pos[0] - 1, pos[1] + 1)) not in visited:
-            visited.add(((pos[0], pos[1]), (pos[0] , pos[1] + 1), (pos[0] - 1, pos[1] + 1)))
+    if not_same_region(map, DIRS.UP, pos) and (is_neighbour(map, pos[0], pos[1], pos[0], pos[1] + 1) and is_neighbour(map, pos[0], pos[1] + 1, pos[0] - 1, pos[1] + 1)):
+        if ((pos[0], pos[1]), (pos[0] , pos[1] + 1), (pos[0] - 1, pos[1] + 1)) not in visited_corners:
+            visited_corners.add(((pos[0], pos[1]), (pos[0] , pos[1] + 1), (pos[0] - 1, pos[1] + 1)))
             sum_corners += 1
     # |_
-    if not_same_region(map, "UP", pos[0], pos[1]) and (is_neighbour(map, pos[0], pos[1], pos[0], pos[1] - 1) and is_neighbour(map, pos[0], pos[1] - 1, pos[0] - 1, pos[1] - 1)):
-        visited.add(((pos[0], pos[1]), (pos[0], pos[1] - 1), (pos[0] - 1, pos[1] - 1)))
-
-        sum_corners += 1
+    if not_same_region(map, DIRS.UP, pos) and (is_neighbour(map, pos[0], pos[1], pos[0], pos[1] - 1) and is_neighbour(map, pos[0], pos[1] - 1, pos[0] - 1, pos[1] - 1)):
+        if ((pos[0], pos[1]), (pos[0], pos[1] - 1), (pos[0] - 1, pos[1] - 1)) not in visited_corners:
+            visited_corners.add(((pos[0], pos[1]), (pos[0], pos[1] - 1), (pos[0] - 1, pos[1] - 1)))
+            sum_corners += 1
     #  _
     # |
-    if not_same_region(map, "DOWN", pos[0], pos[1]) and (is_neighbour(map, pos[0], pos[1], pos[0], pos[1] - 1) and is_neighbour(map, pos[0], pos[1] - 1, pos[0] + 1, pos[1] - 1)):
-        if ((pos[0], pos[1]), (pos[0], pos[1] - 1), (pos[0] + 1, pos[1] - 1)) not in visited:
-            visited.add(((pos[0], pos[1]), (pos[0], pos[1] - 1), (pos[0] + 1, pos[1] - 1)))
+    if not_same_region(map, DIRS.DOWN, pos) and (is_neighbour(map, pos[0], pos[1], pos[0], pos[1] - 1) and is_neighbour(map, pos[0], pos[1] - 1, pos[0] + 1, pos[1] - 1)):
+        if ((pos[0], pos[1]), (pos[0], pos[1] - 1), (pos[0] + 1, pos[1] - 1)) not in visited_corners:
+            visited_corners.add(((pos[0], pos[1]), (pos[0], pos[1] - 1), (pos[0] + 1, pos[1] - 1)))
             sum_corners += 1
     # _
     #  |
-    if not_same_region(map, "DOWN", pos[0], pos[1]) and (is_neighbour(map, pos[0], pos[1], pos[0], pos[1] + 1) and is_neighbour(map, pos[0], pos[1] + 1, pos[0] + 1, pos[1] + 1)):
-        if ((pos[0], pos[1]), (pos[0], pos[1] + 1), (pos[0] + 1, pos[1] + 1)) not in visited:
-            visited.add(((pos[0], pos[1]), (pos[0], pos[1] + 1), (pos[0] + 1, pos[1] + 1)))
+    if not_same_region(map, DIRS.DOWN, pos) and (is_neighbour(map, pos[0], pos[1], pos[0], pos[1] + 1) and is_neighbour(map, pos[0], pos[1] + 1, pos[0] + 1, pos[1] + 1)):
+        if ((pos[0], pos[1]), (pos[0], pos[1] + 1), (pos[0] + 1, pos[1] + 1)) not in visited_corners:
+            visited_corners.add(((pos[0], pos[1]), (pos[0], pos[1] + 1), (pos[0] + 1, pos[1] + 1)))
             sum_corners += 1
     
-    return sum_corners, visited
+    return sum_corners, visited_corners
 
-def find_sides(map, region):
+def calculate_sides(map, region):
     sum = 0
-    visited = set()
+    visited_corners = set()
     for pos in region:
-        corners, new_visited = get_corners(map, pos, visited)
+        corners, new_visited = get_corners(map, pos, visited_corners)
         sum += corners
-        visited = new_visited
+        visited_corners = new_visited
 
     return sum
 
@@ -111,14 +120,14 @@ def find_region(map, y, x):
 
     while len(queue) > 0:
         pos = queue.pop(0)
-        neighbours = find_neighbours(map, pos[0], pos[1])
+        neighbours = find_neighbours(map, pos[Y], pos[X])
 
         for n in neighbours:
             if n not in visited:
                 visited.add(n)
                 queue.append(n)
     
-    sides = find_sides(map, visited)
+    sides = calculate_sides(map, visited)
     return visited, sides
 
 def main():
@@ -131,8 +140,7 @@ def main():
             if (y, x) not in visited:
                 new_visited, per = find_region(map, y, x)
                 visited = visited | new_visited
-                area = calculate_area(new_visited)
-                sum += (per * area)
+                sum += (per * len(new_visited))
 
     print("sum is => ", sum)
 
