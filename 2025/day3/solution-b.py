94e8fbef350bfc09d2d6f9575e7d5f5f479e27f7
Hadi Ansari <hadia@axis.com>
2025-12-03 08:20:01 +0100

    2025: solved day3 part two

--- 2025/day3/solution-b.py
+++ 2025/day3/solution-b.py
@@ -1,6 +1,29 @@
 from reader import read_problem
 
+
+def find_max_joltage(bank):
+    index = 0
+    res = ""
+
+    for c in range(12, 0, -1):
+        max_digit = -1
+        for i in range(index, len(bank) - c + 1):
+            if int(bank[i]) > max_digit:
+                max_digit = int(bank[i])
+                index = i + 1
+        res += str(max_digit)
+
+    return int(res)
+
+
 def main():
-    read_problem("input.txt")
-   
+    banks = read_problem("input.txt")
+    sum = 0
+    for bank in banks:
+        max_joltage = find_max_joltage(bank)
+        # print("Max joltage for bank", bank, "is", max_joltage)
+        sum += max_joltage
+
+    print("The sum of max joltage is:", sum)
+
 main()
