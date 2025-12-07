bf0855dfd3ea0b25b2f86d7d43a17efa4dd77cfe
Hadi Ansari <hadia@axis.com>
2025-12-07 20:04:51 +0100

    2025: refactored day6 part two

--- 2025/day6/solution-b.py
+++ 2025/day6/solution-b.py
@@ -4,28 +4,26 @@ def main():
     lines = read_problem("input.txt")
 
     problems_operation =  {}
-    fromatted_problems = {}
-    new_problems = {}
+    fromated_lines = {}
+    problems = {}
     max_len = {}
+    problem_count = len(lines[0].strip().split())
+
+    # Read operations and lines of numbers and store them in fromated_lines and problems_operation dictionaries
     for i in range(len(lines)):
         if i == len(lines) - 1:
             operations = [x for x in lines[i].strip().split()]
             for j in range(len(operations)):
-                problems_operation[j].append(operations[j])
+                problems_operation[j] = operations[j]
             break
 
         nums = [x for x in lines[i].strip().split()]
-        fromatted_problems[i] = nums
-        for j in range(len(nums)):
-            if j in problems_operation:
-                problems_operation[j].append(nums[j])
-            else:
-                problems_operation[j] = [nums[j]]
+        fromated_lines[i] = nums
 
 
-    max_len = {}
-    for key in fromatted_problems:
-        p = fromatted_problems[key]
+    # Find max length of each problem (each column) and store them in max_len dictionary
+    for key in fromated_lines:
+        p = fromated_lines[key]
         for j in range(len(p)):
             current_num = p[j]
             if j in max_len:
@@ -35,32 +33,34 @@ def main():
                 max_len[j] = len(current_num)
 
 
+    # Slice the colums based on max lenght of that problem (column) and store them in problems dictionary
     for i in range(len(lines)):
         current = 0
         next = -1
         l = lines[i]
         if i == len(lines) - 1:
             break
-        for j in range(len(max_len)):
+
+        for j in range(problem_count):
             next = max_len[j] + current
             num = l[current: next]
             current = next + 1
-            if j in new_problems:
-                new_problems[j].append(num)
+            if j in problems:
+                problems[j].append(num)
             else:
-                new_problems[j] = [num]
+                problems[j] = [num]
 
 
     sum = 0
-    for key in new_problems:
-        current_problem = new_problems[key]
-        operator = problems_operation[key][-1]
+    for key in problems:
+        current_problem = problems[key]
+        operator = problems_operation[key]
         if operator == "*":
             problem_sum = 1
         else:
             problem_sum = 0
         nums = []
-        for j in range(len(current_problem[0])):
+        for j in range(max_len[key]):
             temp_str = ""
             for i in range(len(current_problem)):
                 current_num = current_problem[i]
