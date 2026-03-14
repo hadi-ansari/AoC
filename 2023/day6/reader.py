fc555b472e441f6b5ea0413d7e1a8b27d4676a66
Hadi Ansari <hadi.ansari161@outlook.com>
2026-03-14 18:08:03 +0100

    2023: updating inputs and solving problems with my personal github account

--- 2023/day6/reader.py
+++ 2023/day6/reader.py
@@ -2,10 +2,14 @@ def read_problem(file_name):
     input_file = open(file_name)
 
     content = input_file.readlines()
+
+    races = []
+    times = [int(t) for t in content[0].split()[1:]]
+    distances = [int(t) for t in content[1].split()[1:]]
     
+    for i in range(len(times)):
+        races.append([times[i], distances[i]])
+
     input_file.close()
 
-    times = content[0].split(":")[-1].strip().split()
-    distances = content[1].split(":")[-1].strip().split()
-
-    return times, distances
+    return races
--- 2023/day6/solution-a.py
+++ 2023/day6/solution-a.py
@@ -1,20 +1,21 @@
 from reader import read_problem
 
 def main():
-    times, distances = read_problem("input.txt")
+    races = read_problem("input.txt")
+    result = 1
 
-    time = int("".join(times))
-    distance = int("".join(distances))
+    for i in range(len(races)):
+        time = races[i][0]
+        distance = races[i][1]
+        possible_count = 0
+        for j in range(1, time - 1):
+            speed = j
+            temp_dis = speed * (time - j)
+            if temp_dis > distance:
+                possible_count += 1
 
-    possible_count = 0
+        result *= possible_count
 
-    for j in range(1, time):
-        speed = j
-        temp_dis = speed * (time - j)
-        # print("If hold {} ms it get {} millimeter".format(j, temp_dis))
-        if temp_dis > distance:
-            possible_count += 1
-
-    print(possible_count)
-
+    print("Answer is {}".format(result))
+    
 main()
